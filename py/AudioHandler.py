503e7e4f510c41a5f8f4621ffd4307dbbffb27de
frejaKahle <frejaKahle@gmail.com>
2024-07-22 18:14:23 -0400

    commit 2

--- py/AudioHandler.py
+++ py/AudioHandler.py
@@ -8,11 +8,16 @@ EMPTY_FRAME = bytes(FRAMSIZE)
 class CircularAudioBuffer(BufferedIOBase):
     def __init__(self, extra_audio_frames: int = 0) -> None:
         super().__init__()
-        self.target_size = max(extra_audio_frames,0) + 1
-        self.circlist = CircularList(self.target_size)
-        
-
+        self.target_size: int = max(extra_audio_frames,0) + 1
+        self.circlist: CircularList = CircularList(self.target_size)
+    
+    def resize(self,size):
+        self.target_size = max(size, 0) + 1
+    def __len__(self):
+        return len(self.circlist)
+    
     def read(self):
         return self.circlist.read()
+    
     def write(self):
-        return self.criclist.write()
+        return self.circlist.write()
